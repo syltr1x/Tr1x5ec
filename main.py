a7bb695379451f57ea997eb50dde17a3ece9ec35
syltr1x <syltr1x@proton.me>
2024-03-19 23:57:45 -0300

    change: keys creation

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import cypher
 
 def add_account(net, user, email, pwd, keyName, tkey):
     user = cypher.cifrar('RSA', keyName, user)
-    email = cypher.cifrar('RSA', keyName, email)
     pwd = cypher.cifrar('RSA', keyName, pwd)
+    email = cypher.cifrar('RSA', keyName, email)
+    tkey = cypher.cifrar('RSA', keyName, tkey)    
 
     fu = open(f'data/users/{net}.bin', 'wb')
     fu.write(user) 
@@ -23,7 +24,10 @@ def add_account(net, user, email, pwd, keyName, tkey):
     fe.write(email)
     fe.close()
 
-add_account('google', 'ramon', 'ram@gmail.com', 'aña123', 'google', '090')
+    fk = open(f'data/keys/{net}.bin', 'wb')
+    fk.write(tkey)
+    fk.close()
+
 ########################################
 ################ FRAMES ################
 ########################################
@@ -32,19 +36,35 @@ def add_key_frm():
     Frame = ctk.CTk()
     Frame.title("Añadir Clave | PwD_MaN")
     Frame.geometry("380x380")
+    
+    def change_sizes(secret):
+        if secret == "RSA":
+            length.configure(values=["1024", "2048", "4096", "8192", "16384"])
+        elif secret == "AES":
+            length.configure(values=["64", "128", "256"])
 
-    ktype = ctk.CTkLabel(Frame, text="Tipo de Clave: RSA", width=220, bg_color='gray20', text_color='white')
+    def create_aes(name, size):
+        cypher.create_key('AES', 'user', size, name)
+        cypher.create_key('AES', 'password', size, name)
+        cypher.create_key('AES', 'emails', size, name)
+        cypher.create_key('AES', 'keys', size, name)
+
+    ktype = ctk.CTkComboBox(Frame, values=["RSA", "AES"], width=220, command=change_sizes)
+    ktype.set('Algoritmo')
     ktype.grid(row=0, column=0, padx=5, pady=5, columnspan=2)
     name = ctk.CTkEntry(Frame, placeholder_text="Nombre de la clave", width=100)
     name.grid(row=1, column=0, padx=5, pady=5)
-    length = ctk.CTkOptionMenu(Frame, values=["1024", "2048", "4096", "8192", "16384"], width=100)
+    length = ctk.CTkOptionMenu(Frame, values=[], width=100)
+    length.set('Longitud')
     length.grid(row=1, column=1, padx=5, pady=5)
-    create = ctk.CTkButton(Frame, text="Crear Clave", command=lambda:(cypher.create_key('RSA', int(length.get()), name.get())), width=100)
+    create = ctk.CTkButton(Frame, text="Crear Clave", command=lambda:(cypher.create_key(ktype.get(), 
+            longitud=int(length.get()), name=name.get())), width=100)
     create.grid(row=2, column=0, padx=5, pady=5, columnspan=2)
-    
+
     Frame.mainloop()
 
 def add_account_frm():
+    tkey = cypher.gen_key()
     keysList = [i.split('.')[0] for i in os.listdir('data/keys')]
     keysList = list(set(keysList))
 
@@ -60,11 +80,13 @@ def add_account_frm():
     email_net.grid(row=1, column=0, padx=5, pady=5)
     password_net = ctk.CTkEntry(Frame, placeholder_text="Contraseña", width=150)
     password_net.grid(row=1, column=1, padx=5, pady=5)
+    tkey_net = ctk.CTkLabel(Frame, text=f"Clave: {tkey}", width=150)
+    tkey_net.grid(row=0, column=0, padx=5, pady=5)
     key_sel = ctk.CTkOptionMenu(Frame, values=[i for i in keysList], width=150)
-    key_sel.grid(row=2, column=0, padx=5, pady=5)
+    key_sel.grid(row=2, column=1, padx=5, pady=5)
     add_net = ctk.CTkButton(Frame, text="Añadir Cuenta", command=lambda:(add_account(social_net.get(), user_net.get(), email_net.get(), 
-            password_net.get(), key_sel.get(), cypher.gen_key())), width=150)
-    add_net.grid(row=2, column=1, padx=5, pady=5)
+            password_net.get(), key_sel.get(), tkey)), width=320)
+    add_net.grid(row=3, column=0, padx=5, pady=5, columnspan=2)
 
     Frame.mainloop()
 
@@ -73,11 +95,8 @@ def get_account_frm():
     Frame = ctk.CTk()
     Frame.title("Mostrar Cuenta | PwD_MaN")
     Frame.geometry("600x600")
-    fe = open(f'data/users/google.bin', 'rb')
-    dato = fe.read()
-    fe.close()
-    data = cypher.descifrar('RSA', 'facebook', dato.encode())
-    print(data)
+
+
 
     Frame.mainloop()
 
@@ -100,8 +119,5 @@ btn2.grid(row=0, column=1, padx=5, pady=5)
 
 btn3 = ctk.CTkButton(App, text="Añadir Clave", command=add_key_frm, width=220)
 btn3.grid(row=1, column=0, padx=5, pady=5, columnspan=2)
-
-cypher.create_key('AES', 256, 'user') if os.listdir('data/keys/').count('user_key.bin') == 0 else None
-cypher.create_key('AES', 256, 'password') if os.listdir('data/keys/').count('password_key.bin') == 0 else None
 
 App.mainloop()
